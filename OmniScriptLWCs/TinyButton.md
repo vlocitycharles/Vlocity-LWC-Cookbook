fe037627d265fc747f1d3a27946d3d68e738403d
cmcguinness-vlocity <cmcguinness@vlocity.com>
2020-04-29 22:04:34 -0400

    Tiny Button cleanup

--- OmniScriptLWCs/TinyButton.md
+++ OmniScriptLWCs/TinyButton.md
@@ -2,6 +2,12 @@
 
 [This LWC](tinyButton) has a small UI foot print and behaves somewhat like a set values.  It is intended mostly as a base to start building from, but gives you enough hooks to use as examples.
 
+It does three things:
+
+1. It copies the Field Label from the Custom LWC element (*Me Me Me!*) to the label of the button in the UI.
+2. It sets the value of the element to an object containing "A" and "B" values.
+3. It sets a free-floating value in the data JSON of ["HeyHowdy" to "Hey"](https://www.youtube.com/watch?v=G7X6vPIsE_g).
+
 Here's an example of how it's been added to an OmniScript:
 
 ![tinybutton01](../images/tinybutton01.png)
@@ -10,15 +16,15 @@ You can see the field label specified as Me Me Me!, and how it gets rendered in 
 
 ![](../images/tinybutton02.png)
 
-This screenshot was taken after it was clicked, and you can see two elements have been set in the data JSON.  The first is Step1.CustomLWC1, and the second is HeyHowdy.  These were set in the handler for the on click event on the button:
+This screenshot was taken after it was clicked, and you can see two elements have been set in the data JSON.  The first is Step1.TinyButtonLWC, and the second is HeyHowdy.  These were set in the handler for the on click event on the button:
 
 ```    javascript
 handleClick(event) {
-	//  Set the value of the button in the data JSON (without really knowing "who" I am)
+  //  Set the value of the button in the data JSON (without really knowing "who" I am)
   this.omniUpdateDataJson({"A": "AA", "B":"BB"});
 
-	//  Set some values explicitly in the data JSON
-  this.omniApplyCallResp({"HeyHowdy": "Hi"});
+  //  Set some values explicitly in the data JSON
+  this.omniApplyCallResp({"HeyHowdy": "Hey"});
 }
 ```
 
--- OmniScriptLWCs/tinyButton/tinyButton.js
+++ OmniScriptLWCs/tinyButton/tinyButton.js
@@ -3,54 +3,34 @@
  * 
  * This is a simple LWC that demonstrates three things:
  * 
- * 1. How to get a custom property from the OmniScript element
- * 2. How to set the value of the element to something
- * 3. How to set any element's value in the data JSON
+ * 1. How to set the value of the element to something
+ * 2. How to set any element's value in the data JSON
+ * 3. How to get the text of the label of the element
  * 
  * It is not really intended to be useful as-is, but rather as a
  * starting point for building something more valueable.
  */
 
  // The imports we need as a minimum
-import { LightningElement, track} from 'lwc';
+import { LightningElement, track } from 'lwc';
 import { OmniscriptBaseMixin } from 'vlocityins2/omniscriptBaseMixin';
 
 export default class TinyButton extends OmniscriptBaseMixin(LightningElement) {
 
     // This holds the label which will be shown on the button
     @track buttonlabel = "Click Me";
-
-    // This holds the custom parameters which were passed to us in the definition of the custom LWC
-    parameters = {};
+ 
+ 
 
     // This is called when we're ready to go ... it will see if there's a custom label set
     connectedCallback() {
 
         // For debugging, reference purposes
-        console.log(JSON.stringify(this.omniJsonDef));
+        // console.log(JSON.stringify(this.omniJsonDef));
 
         // We are pretty certain that this will have some value
         this.buttonlabel = this.omniJsonDef.propSetMap.label;
-        this.parseParams();
-
-        if (this.parameters.hasOwnProperty("label")) {
-            this.buttonlabel = this.parameters.label;
-        }
     }
-
-    /**
-     * Get the parameters for the LWC which have been pased in from the OmniScript as metadata
-     */
-    parseParams() {
-        let params = this.omniJsonDef.propSetMap.customAttributes;
-
-        //  Find the values in the list (don't want to be fussy about the order)
-        params.forEach((val) => {
-            this.parameters[val.name] = val.source;
-        });
-    }
-
-
 
     /**
      * Handle the click of the button.
@@ -65,6 +45,6 @@ export default class TinyButton extends OmniscriptBaseMixin(LightningElement) {
         this.omniUpdateDataJson({"A": "AA", "B":"BB"});
 
         //  Set some values explicitly in the data JSON
-        this.omniApplyCallResp({"HeyHowdy": "Hi"});
+        this.omniApplyCallResp({"HeyHowdy": "Hey"});
     }
 }
