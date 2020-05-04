9359479a130141f308490e2aaa0b0ea7602cafa2
cmcguinness-vlocity <cmcguinness@vlocity.com>
2020-05-03 20:50:27 -0400

    First working version if ipLister LWC

--- OtherLWCs/ipLister/lwc/ipLister.js
+++ OtherLWCs/ipLister/lwc/ipLister.js
@@ -1,22 +1,70 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
 import callIP from '@salesforce/apex/ipLWCWrapper.callIP';
 
 
 export default class IpLister extends LightningElement {
-	@api prop1 ="";
-	@api prop2 = false;
+	@api pathToData ="";
 	@api recordId;
+	@api titles = "";
+	@api names = "";
+	@api ipname = "";
 
-	ipResult = '';
+	@api prop1;
+	@api prop2;
+
+	@track ipResult = '';
+	@track rows = [];
+	@track headers = [];
 
 	connectedCallback() {
-		callIP({ ipName: 'lwc_getAccount', input: '{ "recordId": "0015w000029ZeYRAA0"}', options: '{}' })
+		let sInput =  '{ "recordId": "' + this.recordId +'"}';
+		console.log("Input: " + sInput);
+		callIP({ ipName: this.ipname, input: sInput, options: '{}' })
 		.then((result) => {
-			this.ipResult = 'Success: ' + JSON.stringify(result);
+			console.log(typeof(result));
+			console.log('Success: ' + JSON.stringify(result));
+			let parsedData = JSON.parse(result);
+
+			try {
+			parsedData = parsedData[this.pathToData];
+			} catch (e) {
+				this.ipResult = "Unable to find data";
+				return;
+			}
+
+			// Handle singleton vs array data
+			if (Array.isArray(parsedData)) {
+				// do nothing
+			} else {
+				parsedData = [ parsedData ];
+			}
+
+			console.log("Rows: " + JSON.stringify(parsedData));
+
+			let arrayTitles = this.titles.split(',');
+
+			// Generate the headers to display in the page
+			arrayTitles.forEach((k, i) => {
+				// console.log("Title: " + k);
+				this.headers.push({ key : i, value: k.trim()});
+			})
+
+			let arrayNames = this.names.split(',');
+			// Now generate the rows of data
+			parsedData.forEach((row, i) => {
+				let rowdata = [];
+				arrayNames.forEach((k,j) => {
+					k = k.trim();
+					// console.log("key: " + k + ", value: "+row[k]);
+					rowdata.push({key : j, value: row[k]});
+				});
+				this.rows.push({ key: i, value: rowdata});
+			});
 		})
 		.catch((error) => {
 			this.ipResult = 'Error: ' + JSON.stringify(error);
+			console.log(this.ipResult);
 		})
 	}
 }
